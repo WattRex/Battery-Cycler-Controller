b9de621a67fb48c6d1964a72933025c588cea976
RobertoAldea <r.aldea.csic@gmail.com>
2023-08-31 12:12:26 +0100

    Update mid data device to get all arguments needed, and also __init__ of mid data

--- code/src/wattrex_battery_cycler/mid/mid_data/__init__.py
+++ code/src/wattrex_battery_cycler/mid/mid_data/__init__.py
@@ -4,4 +4,6 @@
 This file specifies what is going to be exported from this module.
 '''
 
-from .mid_data_devices import MidDataDeviceStatusE, MidDataDeviceTypeE, MidDataDeviveStatusC
+from .mid_data_devices import MidDataDeviceStatusE, MidDataDeviceTypeE, MidDataDeviveStatusC, \
+                    MidDataDeviceC, MidDataLinkConfSerialC, MidDataLinkConfCanC
+from .mid_data_experiment import MidDataPwrLimitE
--- code/src/wattrex_battery_cycler/mid/mid_data/mid_data_devices.py
+++ code/src/wattrex_battery_cycler/mid/mid_data/mid_data_devices.py
@@ -34,11 +34,11 @@ class MidDataDeviceTypeE(Enum):
     '''
     Allowed power device types in the system.
     '''
-    SOURCE = "Source"
-    BISOURCE = "BiSource"
-    LOAD = "Load"
-    METER = "Meter"
-    EPC     = 'Epc'
+    SOURCE      = "Source"
+    BISOURCE    = "BiSource"
+    LOAD        = "Load"
+    METER       = "Meter"
+    EPC         = 'Epc'
 
 #######################             CLASSES              #######################
 
@@ -99,6 +99,44 @@ class MidDataDeviveStatusC:
         '''
         return self.__status.name
 
+class MidDataLinkConfCanC:
+    """A class method that implements the MIDDataLinkConCanf class .
+    """
+    def __init__(self, can_id: int):
+        self.can_id = can_id
+class MidDataLinkConfSerialC:
+    '''
+    A class method that implements the MIDDataLinkConSerialf class .
+    '''
+    # pylint: disable=too-many-arguments
+    def __init__(self,
+                port: str, separator: str, baudrate: int, bytesize: int,
+                parity: str,
+                stopbits: int, timeout: float, write_timeout: float,
+                inter_byte_timeout: float) -> None:
+        if 'odd' in parity.lower():
+            parity = 'O'
+        elif 'even' in parity.lower():
+            parity = 'E'
+        elif 'none' in parity.lower():
+            parity = 'N'
+        elif 'mark' in parity.lower():
+            parity = 'M'
+        elif 'space' in parity.lower():
+            parity = 'S'
+        else:
+            log.error("Wrong value for parity")
+            raise ValueError("Wrong value for parity")
+        self.port = port
+        self.separator = separator
+        self.baudrate = baudrate
+        self.bytesize = bytesize
+        self.parity = parity
+        self.stopbits = stopbits
+        self.timeout = timeout
+        self.write_timeout = write_timeout
+        self.inter_byte_timeout = inter_byte_timeout
+
 class MidDataDeviceC:
     '''
     A class method that implements the MIDDataDevice class .
@@ -106,10 +144,12 @@ class MidDataDeviceC:
 
     # pylint: disable=too-many-arguments
     def __init__(self, manufacturer : str, model : str, serial_number : str,
-                device_type : MidDataDeviceTypeE, iface_name : str, mapping_names : Dict) -> None:
+                device_type : MidDataDeviceTypeE, iface_name : str, mapping_names : Dict,
+                link_configuration: MidDataLinkConfCanC|MidDataLinkConfSerialC) -> None:
         self.manufacturer : str = manufacturer
         self.model : str = model
         self.serial_number : str  = serial_number
         self.device_type : MidDataDeviceTypeE = device_type
         self.iface_name :str = iface_name
         self.mapping_names : Dict = mapping_names
+        self.link_conf: MidDataLinkConfCanC|MidDataLinkConfSerialC = link_configuration
