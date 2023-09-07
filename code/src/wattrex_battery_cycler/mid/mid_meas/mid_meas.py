335c665416e250c499a3f5d44001732d080f1198
RobertoAldea <r.aldea.csic@gmail.com>
2023-09-07 07:28:46 +0100

    Implemented meas node with shared object

--- code/src/wattrex_battery_cycler/mid/mid_meas/mid_meas.py
+++ code/src/wattrex_battery_cycler/mid/mid_meas/mid_meas.py
@@ -5,10 +5,9 @@ the device and request info from it.
 """
 #######################        MANDATORY IMPORTS         #######################
 from __future__ import annotations
-
+from typing import List
 #######################         GENERIC IMPORTS          #######################
 from threading import Event
-from datetime import datetime
 #######################       THIRD PARTY IMPORTS        #######################
 
 from system_logger_tool import SysLogLoggerC, sys_log_logger_get_module_logger, Logger
@@ -16,56 +15,58 @@ if __name__ == '__main__':
     cycler_logger = SysLogLoggerC(file_log_levels= 'log_config.yaml')
 log: Logger = sys_log_logger_get_module_logger(__name__)
 
-from system_config_tool import sys_conf_read_config_params
 from system_shared_tool import SysShdSharedObjC, SysShdNodeC, SysShdParamsC
 
 #######################          MODULE IMPORTS          #######################
-from ..mid_dabs import MidDabsPwrMeterC
-from ..mid_data import MidDataDeviceTypeE, MidDataDeviceC,\
-            MidDataGenMeasC, MidDataExtMeasC, MidDataAllStatusC
+from ..mid_dabs import MidDabsPwrMeterC #pylint: disable= relative-beyond-top-level
+from ..mid_data import MidDataDeviceC, MidDataGenMeasC, MidDataExtMeasC, MidDataAllStatusC #pylint: disable= relative-beyond-top-level
 #######################          PROJECT IMPORTS         #######################
 
 #######################              ENUMS               #######################
 
 #######################             CLASSES              #######################
 
-class MidMeasNodeC(SysShdNodeC):
+class MidMeasNodeC(SysShdNodeC): #pylint: disable=too-many-instance-attributes
     """Returns a removable version of the DRv command .
 
     Args:
         threading ([type]): [description]
     """
 
-    def __init__(self, cycle_period: float, working_flag : Event, config_file: str,
+    def __init__(self,shared_gen_meas: SysShdSharedObjC, shared_ext_meas: SysShdSharedObjC, #pylint: disable= too-many-arguments
+                 shared_status: SysShdSharedObjC, cycle_period: float, working_flag : Event,
+                 devices: List[MidDataDeviceC],
                  meas_params: SysShdParamsC= SysShdParamsC()) -> None:
         '''
         Initialize the thread node used to update measurements from devices.
         '''
-
         super().__init__(cycle_period, working_flag, meas_params)
-        conf_param = sys_conf_read_config_params(config_file)
         self.working_flag = working_flag
-        self.devices: MidDabsPwrMeterC = MidDabsPwrMeterC([MidDataDeviceC(**conf_param)])
-        self.globlal_gen_meas: SysShdSharedObjC
-        self.globlal_ext_meas: SysShdSharedObjC
-        self.globlal_all_status: SysShdSharedObjC
-        self.__all_status: MidDataAllStatusC
-        self.__gen_meas: MidDataGenMeasC
-        self.__ext_meas: MidDataExtMeasC
+        self.devices: MidDabsPwrMeterC = MidDabsPwrMeterC(devices)
+        self.globlal_gen_meas: SysShdSharedObjC = shared_gen_meas
+        self.globlal_ext_meas: SysShdSharedObjC = shared_ext_meas
+        self.globlal_all_status: SysShdSharedObjC = shared_status
+        self._all_status: MidDataAllStatusC = MidDataAllStatusC()
+        self._gen_meas: MidDataGenMeasC = MidDataGenMeasC(0,0,0)
+        self._ext_meas: MidDataExtMeasC = MidDataExtMeasC()
 
     def sync_shd_data(self) -> None:
         '''Update 
         '''
-        self.__all_status = self.globlal_all_status.merge_exclude_tags(self.__all_status, [])
-        self.__gen_meas = self.globlal_gen_meas.merge_exclude_tags(self.__gen_meas, [])
-        self.__ext_meas = self.globlal_ext_meas.merge_exclude_tags(self.__ext_meas, [])
+        self._all_status = self.globlal_all_status.merge_exclude_tags(self._all_status, [])
+        self._gen_meas = self.globlal_gen_meas.merge_exclude_tags(self._gen_meas, [])
+        self._ext_meas = self.globlal_ext_meas.merge_exclude_tags(self._ext_meas, [])
 
     def process_iteration(self) -> None:
         """Processes a single iteration.
         """
         # Update the measurements and status of the devices.
-        self.devices.update(self.__gen_meas, self.__ext_meas, self.__all_status)
+        self.devices.update(self._gen_meas, self._ext_meas, self._all_status)
         # Sync the shared data with the updated data.
         self.sync_shd_data()
 
-#######################            FUNCTIONS             #######################
+    def close(self) -> None:
+        """Close the thread.
+        """
+        self.devices.close()
+        super().close()
