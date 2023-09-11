2ecb1cfcb3db400146c5e2c57f851a2406722fcf
RobertoAldea <r.aldea.csic@gmail.com>
2023-09-11 07:07:56 +0100

    Added classes and enums to init

--- code/src/wattrex_battery_cycler/mid/mid_data/__init__.py
+++ code/src/wattrex_battery_cycler/mid/mid_data/__init__.py
@@ -6,11 +6,15 @@ This file specifies what is going to be exported from this module.
 
 from .mid_data_devices import MidDataDeviceStatusE, MidDataDeviceTypeE, MidDataDeviveStatusC, \
                     MidDataDeviceC, MidDataLinkConfSerialC, MidDataLinkConfCanC
-from .mid_data_experiment import MidDataPwrLimitE, MidDataPwrModeE, MidDataPwrRangeC
+from .mid_data_experiment import MidDataPwrLimitE, MidDataPwrModeE, MidDataPwrRangeC, MidDataAlarmC,\
+                    MidDataExperimentC, MidDataExpStatusE, MidDataInstructionC, MidDataProfileC
 from .mid_data_common import MidDataAllStatusC, MidDataExtMeasC, MidDataGenMeasC
+from .mid_data_battery import MidDataBatteryC, MidDataLithiumBatC, MidDataRedoxBatC
 
 __all__ = [
     'MidDataDeviceStatusE', 'MidDataDeviceTypeE', 'MidDataDeviveStatusC', 'MidDataDeviceC',
-    'MidDataLinkConfSerialC', 'MidDataLinkConfCanC', 'MidDataPwrLimitE', 'MidDataAllStatusC',
-    'MidDataExtMeasC', 'MidDataGenMeasC', 'MidDataPwrModeE', 'MidDataPwrRangeC'
+    'MidDataLinkConfSerialC', 'MidDataLinkConfCanC', 'MidDataPwrLimitE', 'MidDataPwrModeE',
+    'MidDataPwrRangeC', 'MidDataAlarmC', 'MidDataExperimentC', 'MidDataExpStatusE',
+    'MidDataInstructionC', 'MidDataProfileC', 'MidDataAllStatusC', 'MidDataExtMeasC',
+    'MidDataGenMeasC', 'MidDataBatteryC', 'MidDataLithiumBatC', 'MidDataRedoxBatC'
 ]
