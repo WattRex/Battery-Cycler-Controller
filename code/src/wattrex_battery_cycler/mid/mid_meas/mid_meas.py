ef40fc25c8e7dfdc02c9198bf4c4a26a8fd5249a
marius <marius.crisan@csic.es>
2023-09-14 14:21:34 +0100

    Changes stoping the node and way of updating shared objects

--- code/src/wattrex_battery_cycler/mid/mid_meas/mid_meas.py
+++ code/src/wattrex_battery_cycler/mid/mid_meas/mid_meas.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     cycler_logger = SysLogLoggerC(file_log_levels= 'log_config.yaml')
 log: Logger = sys_log_logger_get_module_logger(__name__)
 
-from system_shared_tool import SysShdSharedObjC, SysShdNodeC, SysShdNodeParamsC
+from system_shared_tool import SysShdSharedObjC, SysShdNodeC, SysShdNodeParamsC, SysShdErrorC
 
 #######################          MODULE IMPORTS          #######################
 from ..mid_dabs import MidDabsPwrMeterC #pylint: disable= relative-beyond-top-level
@@ -34,28 +34,32 @@ class MidMeasNodeC(SysShdNodeC): #pylint: disable=too-many-instance-attributes
     """
 
     def __init__(self,shared_gen_meas: SysShdSharedObjC, shared_ext_meas: SysShdSharedObjC, #pylint: disable= too-many-arguments
-                 shared_status: SysShdSharedObjC, cycle_period: float, working_flag : Event,
+                 shared_status: SysShdSharedObjC, cycle_period: int, working_flag : Event,
                  devices: List[MidDataDeviceC],
                  meas_params: SysShdNodeParamsC= SysShdNodeParamsC()) -> None:
         '''
         Initialize the thread node used to update measurements from devices.
         '''
-        super().__init__(cycle_period, working_flag, meas_params)
+        super().__init__(name= "Meas_Node",cycle_period= cycle_period, working_flag= working_flag, 
+                        node_params= meas_params)
         self.working_flag = working_flag
         self.devices: MidDabsPwrMeterC = MidDabsPwrMeterC(devices)
         self.globlal_gen_meas: SysShdSharedObjC = shared_gen_meas
         self.globlal_ext_meas: SysShdSharedObjC = shared_ext_meas
         self.globlal_all_status: SysShdSharedObjC = shared_status
         self._all_status: MidDataAllStatusC = MidDataAllStatusC()
-        self._gen_meas: MidDataGenMeasC = MidDataGenMeasC(0,0,0)
+        self._gen_meas: MidDataGenMeasC = MidDataGenMeasC(voltage= 0, current= 0, power= 0)
         self._ext_meas: MidDataExtMeasC = MidDataExtMeasC()
 
     def sync_shd_data(self) -> None:
         '''Update 
         '''
-        self._all_status = self.globlal_all_status.merge_exclude_tags(self._all_status, [])
-        self._gen_meas = self.globlal_gen_meas.merge_exclude_tags(self._gen_meas, [])
-        self._ext_meas = self.globlal_ext_meas.merge_exclude_tags(self._ext_meas, [])
+        try:
+            self.globlal_all_status.write(self._all_status)
+            self.globlal_gen_meas.write(self._gen_meas)
+            self.globlal_ext_meas.write(self._ext_meas)
+        except SysShdErrorC as err:
+            log.error(f"Failed to sync shared data: {err}")
 
     def process_iteration(self) -> None:
         """Processes a single iteration.
@@ -65,8 +69,8 @@ class MidMeasNodeC(SysShdNodeC): #pylint: disable=too-many-instance-attributes
         # Sync the shared data with the updated data.
         self.sync_shd_data()
 
-    def close(self) -> None:
+    def stop(self) -> None:
         """Close the thread.
         """
         self.devices.close()
-        super().close()
+        super().stop()
