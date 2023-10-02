8a83c607e3f31c924b237d811102f90f0f71f204
RobertoAldea <r.aldea.csic@gmail.com>
2023-10-02 07:43:01 +0100

    Added mapping, update for mapping to be more homogeneus key-value is database-internal attributes

--- code/src/wattrex_battery_cycler/mid/mid_str/mid_str_mapping.py
+++ code/src/wattrex_battery_cycler/mid/mid_str/mid_str_mapping.py
@@ -11,41 +11,44 @@ from typing import Dict
 
 #######################       THIRD PARTY IMPORTS        #######################
 #######################    SYSTEM ABSTRACTION IMPORTS    #######################
+from system_logger_tool import sys_log_logger_get_module_logger
+log = sys_log_logger_get_module_logger(__name__)
 #######################       LOGGER CONFIGURATION       #######################
 #######################          MODULE IMPORTS          #######################
 #######################          PROJECT IMPORTS         #######################
 #######################              ENUMS               #######################
 mapping_gen_meas: Dict[str, str] = {
-    "voltage": 'Voltage',
-    "current": 'Current',
-    "power": 'Power',
-    "pwr_mode": 'PwrMode',
+    "Voltage": 'voltage',
+    "Current": 'current',
+    "Power": 'power',
+    "PwrMode": 'pwr_mode',
+    "InstrID": 'instr_id',
 }
 
-# mapping_experiment: Dict[str, str] = {
-#     "exp_id": 'ExpID',
-#     "name": 'Name',
-#     "status": 'Status',
-#     "date_begin": 'DateBegin',
-#     "date_finish": 'DateFinish'
-# }
+mapping_experiment: Dict[str, str] = {
+    'ExpID': "exp_id",
+    "Name": 'name',
+    "Status": 'status',
+    'DateBegin': "date_begin",
+    'DateFinish': "date_finish"
+}
 
 mapping_alarm: Dict[str, str] = {
-    "timestamp": 'Timestamp',
-    "code": 'Code',
-    "value": 'Value',
+    "Timestamp": 'timestamp',
+    "Code": 'code',
+    "Value": 'value',
 }
 
 mapping_status: Dict[str, str] = {
-    "exp_id": 'ExpID',
-    "status": 'Status',
-    "date": 'Date'
+    'ExpID': "exp_id",
+    'Status': "status", 
+    'Date': "date"
 }
 
 map_cs_db: Dict[str, str] = {
     'Name': 'name',
-    'CSID': 'cycler_id'}
-    # 'Deprecated': 'deprecated'}
+    'CSID': 'cs_id',
+    'Deprecated': 'deprecated'}
 
 map_dev_db: Dict[str, str] = {
     'Manufacturer': 'manufacturer',
@@ -76,6 +79,22 @@ map_inst_db: Dict[str, str] = {
     'LimitType': 'limit_type',
     'LimitPoint': 'limit_ref'}
 
+map_link_conf_db: List[str] = {
+    'separator', 'baudrate', 'bytesize', 'parity', 'stopbits', 'timeout', 'write_timeout',
+    'inter_byte_timeout'}
+
+map_instr_modes: Dict[str, int] = {
+    'WAIT'      : 0,
+    'CV_MODE'   : 1,
+    'CC_MODE'   : 2,
+    'CP_MODE'   : 3}
+
+map_instr_limit_modes: Dict[str, int] = {
+    'TIME'      : 0,
+    'VOLTAGE'   : 1,
+    'CURRENT'   : 2,
+    'POWER'   : 3}
+
 #######################             CLASSES              #######################
 def remap_dict(data: dict, mapping: dict) -> dict:
     """Change the key names of the data dictionary with the names of the mapping dictionary.
@@ -86,4 +105,6 @@ def remap_dict(data: dict, mapping: dict) -> dict:
     Returns:
         dict: [description]
     """
-    return {mapping[k]:data[k] for k in data}
+    log.debug(f"Remapping data {data} with mapping keys: {mapping}")
+    log.debug(f"Returning: {{n_key:data[key] for key, n_key in zip(data.keys(), mapping.keys())}}")
+    return {n_key:data[key] for key, n_key in zip(data.keys(), mapping.keys())}
