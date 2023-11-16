dd00f814dc3e4051ad61d09fba0b18df730fb704
RobertoAldea <r.aldea.csic@gmail.com>
2023-11-16 12:18:48 +0100

    Changes for test and logging for test

--- code/cycler/tests/test_mid_pwr.py
+++ code/cycler/tests/test_mid_pwr.py
@@ -19,15 +19,14 @@ main_logger = SysLogLoggerC(file_log_levels="devops/cycler/log_config.yaml",
 log: Logger = sys_log_logger_get_module_logger(name="test_mid_pwr")
 from system_shared_tool import SysShdSharedObjC, SysShdNodeStatusE
 #######################       THIRD PARTY IMPORTS        #######################
-from can_sniffer import DrvCanNodeC
 from wattrex_battery_cycler_datatypes.cycler_data import (CyclerDataDeviceC, CyclerDataDeviceTypeE,
                 CyclerDataLinkConfC, CyclerDataGenMeasC, CyclerDataExtMeasC, CyclerDataAllStatusC,
                 CyclerDataMergeTagsC, CyclerDataPwrRangeC, CyclerDataInstructionC,
                 CyclerDataPwrModeE, CyclerDataPwrLimitE, CyclerDataAlarmC, CyclerDataExpStatusE)
 #######################          MODULE IMPORTS          #######################
 sys.path.append(os.getcwd()+'/code/cycler/')
-from src.wattrex_battery_cycler.mid.mid_pwr import MidPwrControlC
-from src.wattrex_battery_cycler.mid.mid_meas import MidMeasNodeC
+from src.wattrex_battery_cycler.mid.mid_pwr import MidPwrControlC #pylint: disable= import-error
+from src.wattrex_battery_cycler.mid.mid_meas import MidMeasNodeC #pylint: disable= import-error
 
 #######################              CLASS               #######################
 
@@ -50,7 +49,7 @@ class TestChannels:
         sleep(2)
 
     @fixture(scope="function", autouse=False)
-    def set_environ(self, request): #pylint: disable= too-many-locals
+    def set_environ(self, request): #pylint: disable= too-many-locals, too-many-statements
         """Setup the environment variables and start the process .
 
         Args:
@@ -74,21 +73,6 @@ class TestChannels:
                     "temp_anod": 3,
                     "temp_amb": 4
                 },
-            },
-            "BMS": {
-                "dev_db_id": 4,
-                "device_type": "Bms",
-                "iface_name": 4,
-                "manufacturer": "abc",
-                "model" : "123",
-                "serial_number" : "12311",
-                "mapping_names" : {'vcell1': 1, 'vcell2': 2, 'vcell3': 3,
-                        'vcell4': 4, 'vcell5': 5, 'vcell6': 6,
-                        'vcell7': 7, 'vcell8': 8, 'vcell9': 9,
-                        'vcell10': 10, 'vcell11': 11, 'vcell12': 12,
-                        'vstack': 13, 'temp1': 14, 'temp2': 15,
-                        'temp3': 16, 'temp4': 17, 'pres1': 18,
-                        'pres2': 19},
             },
             "SOURCE": {
                 "dev_db_id": 18,
@@ -116,7 +100,6 @@ class TestChannels:
             }
         }
         devices: List[CyclerDataDeviceC] = []
-        # request = ['EPC']
         if set(['SOURCE','LOAD']).issubset(request.param):
             conf_param_dev1 = conf_param['SOURCE']
             conf_param_dev2 = conf_param['LOAD']
@@ -131,30 +114,17 @@ class TestChannels:
             conf_param_epc = conf_param[next(iter(conf_param))]
             conf_param_epc['device_type'] = CyclerDataDeviceTypeE(conf_param_epc['device_type'])
             devices: List[CyclerDataDeviceC] = [CyclerDataDeviceC(**conf_param_epc)]
-        ### ADD EXTRA METERS
-        if {'BMS'} <= conf_param.keys():
-            conf_param = conf_param['BMS']
-            conf_param['device_type'] = CyclerDataDeviceTypeE(conf_param['device_type'])
-            devices.append(CyclerDataDeviceC(**conf_param))
         log.info(msg=f"Devices: {devices}")
-        tags = CyclerDataMergeTagsC(status_attrs= ['pwr_dev', 'pwr_mode'],
-                                    gen_meas_attrs= ['voltage', 'current', 'power'],
-                                    ext_meas_attrs= ['hs_voltage_1', 'temp_body_2', 'temp_anod_3',
-                                                'temp_amb_4', 'vcell1_1', 'vcell2_2', 'vcell3_3',
-                                                'vcell4_4', 'vcell5_5', 'vcell6_6', 'vcell7_7',
-                                                'vcell8_8', 'vcell9_9', 'vcell10_10', 'vcell11_11',
-                                                'vcell12_12', 'vstack_13', 'temp1_14', 'temp2_15',
-                                                'temp3_16', 'temp4_17', 'pres1_18', 'pres2_19'])
-        self._meas_working_flag = Event()
+        tags = CyclerDataMergeTagsC(status_attrs= [],
+                                    gen_meas_attrs= ['instr_id'],
+                                    ext_meas_attrs= [])
+        self._meas_working_flag = Event() #pylint: disable= attribute-defined-outside-init
         self._meas_working_flag.set()
-        aux_ext_meas = CyclerDataExtMeasC()
-        # for attr in tags.ext_meas_attrs:
-        #     setattr(aux_ext_meas, attr, None)
         gen_meas: SysShdSharedObjC = SysShdSharedObjC(CyclerDataGenMeasC())
-        ext_meas: SysShdSharedObjC = SysShdSharedObjC(aux_ext_meas)
+        ext_meas: SysShdSharedObjC = SysShdSharedObjC(CyclerDataExtMeasC())
         all_status: SysShdSharedObjC = SysShdSharedObjC(CyclerDataAllStatusC())
         mid_meas_node = MidMeasNodeC(shared_gen_meas = gen_meas, shared_ext_meas = ext_meas,
-                                     shared_status = all_status, cycle_period = 100,
+                                     shared_status = all_status, cycle_period = 120,
                                      working_flag = self._meas_working_flag, devices = devices,
                                      excl_tags= tags)
 
@@ -164,34 +134,30 @@ class TestChannels:
                                 ref= 1000, limit_type= CyclerDataPwrLimitE.TIME, limit_ref= 10000),
                     CyclerDataInstructionC(instr_id= 2, mode= CyclerDataPwrModeE.WAIT, ref= 10000,
                                 limit_type= CyclerDataPwrLimitE.TIME, limit_ref= 10000)]
-        self.epc_pwr = MidPwrControlC(devices= devices, alarm_callback= alarm_callback,
+        self.epc_pwr = MidPwrControlC(devices= devices, alarm_callback= alarm_callback, #pylint: disable= attribute-defined-outside-init
                                       battery_limits= None, instruction_set= None)
         self.epc_pwr.set_new_experiment(instructions= instructions, bat_pwr_range= battery_info)
         exp_status = CyclerDataExpStatusE.QUEUED
         try:
             mid_meas_node.start()
-            i=0
             while mid_meas_node.status is not SysShdNodeStatusE.OK:
                 sleep(1)
+                log.info(f"Waiting for mid_meas_node to start. Status: {mid_meas_node.status.name}")
             while exp_status is not CyclerDataExpStatusE.FINISHED:
                 tic = time()+1
                 local_gen_meas = gen_meas.read()
                 log.info(f"Measuring: {local_gen_meas.voltage}mV and {local_gen_meas.current}mA")
-                # log.info(f"Measuring: hs_voltage =  {ext_meas.read().hs_voltage_1} mV")
                 data_ext = ext_meas.read()
-                epc_ext = [f"{attr}: {data_ext.__dict__[attr]}"  for attr in tags.ext_meas_attrs[0:4]]
+                epc_ext = [f"{attr}: {data_ext.__dict__[attr]}"  for attr in data_ext.__dict__.keys()]
                 log.info(f"Measuring: external measures epc=   {epc_ext}")
-                # bms_ext = [f"{attr}: {data_ext.__dict__[attr]}"  for attr in tags.ext_meas_attrs[4:]]
-                # log.info(f"Measuring: external measures bms=   {bms_ext}")
                 data_status = all_status.read()
                 log.info(f"Status epc:{data_status.pwr_dev.name}")
                 log.info(f"Mode epc:{data_status.pwr_mode.name}")
-                # log.info(f"Status bms:{data_status.extra_meter_4.name}")
                 if data_status.pwr_dev.error_code != 0:
                     log.error((f"Reading error {data_status.pwr_dev.name}, "
                               f"code: {data_status.pwr_dev.error_code}"))
 
-                # ###################### SYNC WITH PWR AND EXECUTE INSTRUCTION ######################
+                ####################### SYNC WITH PWR AND EXECUTE INSTRUCTION ######################
                 self.epc_pwr.update_local_data(gen_meas.read(), all_status.read())
                 exp_status, inst_id =self.epc_pwr.process_iteration()
                 log.info(f"Status: {exp_status.name}, instruction: {inst_id}")
@@ -219,8 +185,9 @@ class TestChannels:
             set_environ ([type]): [description]
             config ([type]): [description]
         """
-        log.debug(msg="1. Test mid meas")
+        log.debug(msg="1. Test mid power")
 
 def alarm_callback(alarm: CyclerDataAlarmC) -> None:
+    '''Callback for alarm'''
     log.error("This is a test for alarm callback")
     log.error(f"Received {alarm.__dict__}")
