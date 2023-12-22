2b4d1eb3533ef43dd67939c5b9c09335fd91d86b
RobertoAldea <r.aldea.csic@gmail.com>
2023-12-22 12:58:15 +0100

    Minor changes in dockerfiles, deploy and runs

--- devops/db_sync/run_db_sync.py
+++ devops/db_sync/run_db_sync.py
@@ -20,9 +20,9 @@ cycler_logger = SysLogLoggerC(file_log_levels='./config/db_sync/log_config.yaml'
 log: Logger = sys_log_logger_get_module_logger(__name__)
 
 #######################          MODULE IMPORTS          #######################
-sys.path.append(os.path.dirname(__file__)+'/../')
-#from code.db_sync.src.wattrex_cycler_db_sync import DbSyncNodeC
-from wattrex_cycler_db_sync import DbSyncNodeC
+sys.path.append(os.path.dirname(__file__)+'/../../code')
+from db_sync.src.wattrex_cycler_db_sync import DbSyncNodeC
+# from wattrex_cycler_db_sync import DbSyncNodeC
 
 #######################          PROJECT IMPORTS         #######################
 
