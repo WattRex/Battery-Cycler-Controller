d8219434aa0de4c61da629dc5e57c2d483956cfa
RobertoAldea <r.aldea.csic@gmail.com>
2024-02-06 12:26:54 +0100

    Added default serial period

--- code/cycler/src/wattrex_battery_cycler/mid/mid_meas/context.py
+++ code/cycler/src/wattrex_battery_cycler/mid/mid_meas/context.py
@@ -26,6 +26,7 @@ from system_config_tool import sys_conf_update_config_params
 
 DEFAULT_NODE_PERIOD: int        = 120 # Express in milliseconds
 DEFAULT_NODE_NAME: str          = 'MEAS'
+DEFAULT_SERIAL_PERIOD: int      = 900 # Express in milliseconds, it has to be over the 800ms
 
 CONSTANTS_NAMES = ('DEFAULT_NODE_PERIOD', 'DEFAULT_NODE_NAME')
 sys_conf_update_config_params(context=globals(),
